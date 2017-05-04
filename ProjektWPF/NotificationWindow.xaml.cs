92020fe4b18b5c28183a6c225fde9908ac76c007
Paweł Radgowski <pawello1001@gmail.com>
2017-05-04 17:49:18 +0200

    Init commit

--- ProjektWPF/NotificationWindow.xaml.cs
+++ ProjektWPF/NotificationWindow.xaml.cs
@@ -10,25 +10,19 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace ProjektWPF
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for NotificationWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class NotificationWindow : Window
     {
-        public MainWindow()
+        public NotificationWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            NotificationWindow window = new NotificationWindow();
-            window.Show();
         }
     }
 }
